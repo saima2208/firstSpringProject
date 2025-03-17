90898fe0f596313fed960880bcbd2a01d1e832c8
saima <saimasonya22@gmail.com>
2025-03-18 01:00:50 +0600

    spring_s

---
+++ spring_s/src/main/java/org/isdb/config/config.java
@@ -0,0 +1,18 @@
+package org.isdb.config;
+
+
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+
+@Configuration
+public class config {
+	@Bean
+	public ObjectMapper objectMapper() {
+		return new ObjectMapper().registerModule(new JavaTimeModule());
+	}
+}
