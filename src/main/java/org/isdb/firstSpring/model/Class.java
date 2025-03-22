d952d8487b4cf04ec5d51ba98620df1e49b56a34
Java Student PC-7 <saimasonya22@gmail.com>
2025-03-22 10:39:12 +0600

    teacher and class add

---
+++ src/main/java/org/isdb/firstSpring/model/Class.java
@@ -0,0 +1,32 @@
+package org.isdb.firstSpring.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity(name = "T_CLASS")
+public class Class {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+    @Column(nullable=false,length=50)
+    private String name;
+    @OneToOne
+    @JoinColumn(name = "class_teacher",referencedColumnName = "id" ,nullable=false)
+    
+    private Teacher classTeacher;
+    @Column(name= "room_number",nullable=false,length=10,unique = true)
+    private Integer roomNumber;
+}
